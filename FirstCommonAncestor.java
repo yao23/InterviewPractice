533a9d7a61bb558bd17762a0c9dce2a6e2f604cf
Yao <myspiritcrazy@gmail.com>
2014-02-03 19:32:12 -0800

    add solution with parent pointer

--- FirstCommonAncestor.java
+++ FirstCommonAncestor.java
@@ -1,34 +1,28 @@
+import java.util.HashSet;
+import java.util.Set;
 
 public class FirstCommonAncestor {
-	static Node commonAncestor(Node one, Node two) {
-		if( one == null )
-	    	return two;
-		if( two == null )
-	    	return one;
-		if( FindInChild(one, two) )
-			return one;
-		if( FindInChild(two, one) )
-		return two;
-		Node OneParent = one, TwoParent = two;
+	public static Node commonAncestor(Node one, Node two) {
+		if( one == null || two == null )
+	    	return null;
+		Set<Node> nodes = new HashSet<Node>();
 		
-		while( OneParent != null && TwoParent != null ) {
-			if( OneParent == TwoParent )
-				return OneParent;
-			OneParent = OneParent.parent;
-			TwoParent = TwoParent.parent;
+		while( one != null || two != null ) {
+			if( one != null ) {
+				if( nodes.add(one) ) 
+					one = one.parent;
+				else 
+					return one;
+			}
+			if( two != null ) {
+				if( nodes.add(two) )
+					two = two.parent;
+				else
+					return two;
+			}
 		}
-
+		
 		return null;
-	}
-
-	private static boolean FindInChild(Node root, Node target) {
-		if( root == null )
-			return false;
-		if( root == target )
-			return true;
-		if( root.left != null || root.right != null )
-			return FindInChild(root.left, target) || FindInChild(root.right, target);
-		return false;
 	}
 	
 	public static TreeNode LCA(TreeNode root, TreeNode a, TreeNode b){
@@ -80,10 +74,10 @@ public class FirstCommonAncestor {
 		TreeNode res2 = LCA(A, C, G);
 		TreeNode res3 = LCA(A, C, C);
 		TreeNode res4 = LCA(A, B, A);
-		System.out.println("First Common Ancestor of D and F is: " + res1.c);
-		System.out.println("First Common Ancestor of C and G is: " + res2.c);
-		System.out.println("First Common Ancestor of C and C is: " + res3.c);
-		System.out.println("First Common Ancestor of B and A is: " + res4.c);
+		System.out.println("Bottom Up First Common Ancestor of D and F is: " + res1.c);
+		System.out.println("Bottom Up First Common Ancestor of C and G is: " + res2.c);
+		System.out.println("Bottom Up First Common Ancestor of C and C is: " + res3.c);
+		System.out.println("Bottom Up First Common Ancestor of B and A is: " + res4.c);
 		
 		TreeNode res2_1 = LCA2(A, D, F);
 		TreeNode res2_2 = LCA2(A, C, G);
@@ -93,22 +87,38 @@ public class FirstCommonAncestor {
 		System.out.println("Top Down First Common Ancestor of C and G is: " + res2_2.c);
 		System.out.println("Top Down First Common Ancestor of C and C is: " + res2_3.c);
 		System.out.println("Top Down First Common Ancestor of B and A is: " + res2_4.c);
+		
+		Node A2 = new Node('A');
+		Node B2 = new Node('B');
+		Node C2 = new Node('C');
+		Node D2 = new Node('D');
+		Node E2 = new Node('E');
+		Node G2 = new Node('F');
+		Node F2 = new Node('G');
+		A2.left = B2; A2.right = C2; A2.parent = null;
+		B2.left = D2; B2.right = F2; B2.parent = A2; C2.parent = A2;
+		D2.left = G2; D2.right = E2; D2.parent = B2; E2.parent = D2;
+		F2.parent = B2; G2.parent = D2;
+		Node res3_1 = commonAncestor(D2, F2);
+		Node res3_2 = commonAncestor(C2, G2);
+		Node res3_3 = commonAncestor(C2, C2);
+		Node res3_4 = commonAncestor(B2, A2);
+		System.out.println("With parent pointer, First Common Ancestor of D and F is: " + res3_1.c);
+		System.out.println("With parent pointer, First Common Ancestor of C and G is: " + res3_2.c);
+		System.out.println("With parent pointer, First Common Ancestor of C and C is: " + res3_3.c);
+		System.out.println("With parent pointer, First Common Ancestor of B and A is: " + res3_4.c);
 	}
 }
  
 class Node {
  
-	final char c;
-	final Node parent;
-	final Node left;
-	final Node right;
+	char c;
+	Node parent;
+	Node left;
+	Node right;
  
- 
-	public Node(char c, Node parent, Node left, Node right) {
+	public Node(char c) {
     	this.c = c;
-		this.parent = parent;
-    	this.left = left;
-    	this.right = right;
 	}
  
 	boolean isRoot() {
